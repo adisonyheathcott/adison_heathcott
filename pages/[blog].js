1682c64116c77ad5cebb533ba992cbbd2b25e993
Yuri0011 <adisonhtt@gmail.com>
2021-02-09 00:29:47 -0600

    Fix

--- pages/[blog].js
+++ pages/[blog].js
@@ -18,12 +18,12 @@ const Blog = ({content, data}) => {
     return (
         <div id="blog-div">
             <div id="blog-div-top">
-                <Link href={"/"}>
+                <Link href={"/adison_heathcott"}>
                     <button>
                         <FontAwesomeIcon id="home-icon" icon={['fas', 'home']} size='2x'/>
                     </button>
                 </Link>
-                <Link href={"/posts"}>
+                <Link href={"/adison_heathcott/posts"}>
                     <button>
                         <FontAwesomeIcon id="posts-icon" icon={['fas', 'book']} size='2x'/>
                     </button>
