e84759e7c9b8b00f8625b85083c24618b0c067f7
adisonyheathcott <adisonhtt@gmail.com>
2021-02-15 00:28:36 -0600

    +osdev-intro

--- pages/[blog].js
+++ pages/[blog].js
@@ -1,8 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import matter from 'gray-matter';
 import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import Link from 'next/link';
+import Head from 'next/head';
 
 const CodeBlock = ({ language, value }) => {
     return (
@@ -16,32 +18,38 @@ const Blog = ({content, data}) => {
     const frontmatter = data;
 
     return (
-        <div id="blog-div">
-            <div id="blog-div-top">
-                <Link href="/" as={ process.env.BACKEND_URL + '/' }>
-                    <button>
-                        <FontAwesomeIcon id="home-icon" icon={['fas', 'home']} size='2x'/>
-                    </button>
-                </Link>
-                <Link href="/posts" as={ process.env.BACKEND_URL + '/posts' }>
-                    <button>
-                        <FontAwesomeIcon id="posts-icon" icon={['fas', 'book']} size='2x'/>
-                    </button>
-                </Link>
-            </div>
+        <>
+            <Head>
+                <title>{frontmatter.title}</title>
+            </Head>
+            <div id="blog-div">
+                <div id="blog-div-top">
+                    <Link href="/" as={ process.env.BACKEND_URL + '/' }>
+                        <button>
+                            <FontAwesomeIcon id="home-icon" icon={['fas', 'home']} size='2x'/>
+                        </button>
+                    </Link>
+                    <Link href="/posts" as={ process.env.BACKEND_URL + '/posts' }>
+                        <button>
+                            <FontAwesomeIcon id="posts-icon" icon={['fas', 'book']} size='2x'/>
+                        </button>
+                    </Link>
+                </div>
 
-            <div id="blog-div-blog">
-                <h1>{frontmatter.title}</h1>
-                <h3>{frontmatter.description}</h3>
-                <h6 style={{color: 'black', margin: '10px 0 15px 0'}}>Published: {frontmatter.date}</h6>
+                <div id="blog-div-blog">
+                    <h1>{frontmatter.title}</h1>
+                    <h3>{frontmatter.description}</h3>
+                    <h6 style={{color: 'black', margin: '10px 0 15px 0'}}>Published: {frontmatter.date}</h6>
 
-                <ReactMarkdown
-                    escapeHtml={true}
-                    source={content}
-                    renderers={{ code: CodeBlock }}
-                />
+                    <ReactMarkdown
+                        plugins={[gfm]}
+                        escapeHtml={true}
+                        source={content}
+                        renderers={{ code: CodeBlock }}
+                    />
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
